743c59214fc39b8d115e4687e192cda8279dc912
Mengyue125 <96225244+Mengyue125@users.noreply.github.com>
2022-11-06 10:28:01 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -13,9 +13,51 @@ $(function(){
 			$("#main").fadeIn();
 		})
 		$(".old_human").eq(1).animate({
-			top:2000
+			top:'100%'
 		})
 	})
 	$("#main").hide();
+	$(".time").hide();
+	$(".blessing").hide();
 	
+	
+	function time(){
+		//获取当前时间  
+        var date = new Date();  
+        var date_next = new Date(2022,10,6,11);
+		var temp=(date_next-date)/1000;
+		var day = parseInt(temp/60/60/24);
+		var hours = parseInt(temp/60/60%24);
+		var minutes = parseInt(temp/60%60);
+		var seconds = parseInt(temp%60);
+		$(".day").html(day)
+		$(".hours").html(hours)
+		$(".minutes").html(minutes)
+		$(".seconds").html(seconds)
+		if(day == 0 && hours == 0 && minutes == 0 & seconds == 0){
+			$(".Gifts").css({
+				animationPlayState: 'running'
+			})
+			clearInterval(timer)
+			setTimeout(function(){
+				$(".time").animate({
+					left:'100%'
+				},"slow",function(){
+					$(".time").hide();
+					$(".blessing").show();
+				})
+			},4000)
+		}
+	}
+	time();
+	var timer = setInterval(function(){
+		time()
+	},1000)
+	
+	$(".go").click(function(){
+		$(this).hide();
+		$("#main").fadeOut(700,function(){
+			$(".time").fadeIn();
+		});
+	})
 })
